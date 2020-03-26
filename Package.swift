9c8814c427c9ab458529b06a65b833d430dceb5c
Marko Justinek <marko.justinek@gmail.com>
2020-03-26 19:42:07 +1100

    Add SPM configuration file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+  name: "PACTSwift",
+  platforms: [
+    .macOS(.v10_12), .iOS(.v12), .tvOS(.v12)
+  ],
+  products: [
+    .library(name: "pact-swift", targets: ["PACTSwift"])
+  ],
+  dependencies: [ ],
+  targets: [
+    .target(
+      name: "PACTSwift",
+      dependencies: [],
+      path: "./Sources"
+    )
+  ]
+)
