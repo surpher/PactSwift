ece61306a11dd6289d7f2dcc8722ba83f9325196
Marko Justinek <marko.justinek@gmail.com>
2020-03-26 17:38:46 +1100

    Rearrange project structure following SPM template

---
+++ Tests/Headers/PACTSwiftTests.swift
@@ -0,0 +1,13 @@
+//
+//  PACTSwiftTests.swift
+//  PACTSwiftTests
+//
+//  Created by Marko Justinek on 26/3/20.
+//  Copyright © 2020 PACT Foundation. All rights reserved.
+//
+
+import XCTest
+@testable import PACTSwift
+
+class PACTSwiftTests: XCTestCase { }
+
