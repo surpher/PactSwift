78939eba2d5accb0098e77d428286bc55879a5f1
surpher <marko.justinek@gmail.com>
2020-03-31 18:39:18 +1100

    (fix) Linting

---
+++ Sources/Definitions/PACTHTTPMethod.swift
@@ -0,0 +1,21 @@
+//
+//  PACTHTTPMethod.swift
+//  PACTSwift
+//
+//  Created by Marko Justinek on 31/3/20.
+//  Copyright © 2020 PACT Foundation. All rights reserved.
+//
+
+public enum PACTHTTPMethod: String {
+
+	case GET = "get"
+	case HEAD = "head"
+	case POST = "post"
+	case PUT = "put"
+	case PATCH = "patch"
+	case DELETE = "delete"
+	case TRACE = "trace"
+	case CONNECT = "connect"
+	case OPTIONS = "options"
+
+}
