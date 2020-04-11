5602e4aa92c9cbec7f8d7b807436399a284cb937
surpher <marko.justinek@gmail.com>
2020-04-12 09:16:45 +1000

    wip(Services) Setup file structure

---
+++ PactServices/Sources/PactMockServer/PactMockServer.swift
@@ -0,0 +1,11 @@
+//
+//  PactMockServer.swift
+//  PactServices
+//
+//  Created by Marko Justinek on 12/4/20.
+//  Copyright © 2020 Pact Foundation. All rights reserved.
+//
+
+import Foundation
+
+class PactMockServer { }
