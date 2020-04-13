6a3ad59ed24844ae73429273ba0304b328d225ab
Marko Justinek <marko.justinek@gmail.com>
2020-04-13 15:24:42 +1000

    fix(build) Only support iOS architecture (for now)
    Rust is still struggling building for Mac Catalyst so supporting only iOS architectures.

--- PactMockServer/Package.swift
+++ PactMockServer/Package.swift
@@ -3,16 +3,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "PactMockServer",
-    products: [
-        .library(name: "PactMockServer", targets: ["PactMockServer"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "PactMockServer",
-            dependencies: [],
-            path: "./Sources"
-        ),
-    ]
+	name: "PactMockServer",
+	products: [
+		.library(
+			name: "PactMockServer",
+			targets: ["PactMockServer"]
+		),
+	],
+	dependencies: [],
+	targets: [
+		.target(
+			name: "PactMockServer",
+			dependencies: [],
+			path: "./Sources"
+		),
+	]
 )
