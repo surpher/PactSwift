1c8ff1c540cc4a153eb276b7c932fde874e994cb
Marko Justinek <marko.justinek@gmail.com>
2020-09-10 21:09:59 +1000

    Add Matcher as namespace placeholder

---
+++ Sources/Matchers/Matcher.swift
@@ -0,0 +1,14 @@
+//
+//  Matcher.swift
+//  PactSwift
+//
+//  Created by Marko Justinek on 10/9/20.
+//  Copyright © 2020 PACT Foundation. All rights reserved.
+//
+
+import Foundation
+
+public struct Matcher {
+	// This is a namespace placeholder.
+	// Implement any matchers as `Struct`s in an Matcher extension.
+}
