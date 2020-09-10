e135e69a35c422c4eb276bb7152e9cfcf902a28d
Marko Justinek <marko.justinek@gmail.com>
2020-09-10 22:04:45 +1000

    Refactor unit tests
    Simplifies the unit tests by declaring them with `throws` and avoidng do catch blocks

--- Tests/AnyEncodableTests.swift
+++ Tests/AnyEncodableTests.swift
@@ -24,114 +24,78 @@ import XCTest
 
 class AnyEncodableTests: XCTestCase {
 
-	func testEncodableWrapper_Handles_StringValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": "Bar"]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":"Bar"}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_StringValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": "Bar"]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":"Bar"}"#)
 	}
 
-	func testEncodableWrapper_Handles_IntegerValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": 123]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":123}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_IntegerValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": 123]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":123}"#)
 	}
 
-	func testEncodableWrapper_Handles_DoubleValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": Double(123.45)]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":123.45}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_DoubleValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": Double(123.45)]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":123.45}"#)
 	}
 
-	func testEncodableWrapper_Handles_DecimalValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": Decimal(string: "123.45")]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":123.45}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_DecimalValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": Decimal(string: "123.45")]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":123.45}"#)
 	}
 
-	func testEncodableWrapper_Handles_BoolValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": true]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":true}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_BoolValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": true]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":true}"#)
 	}
 
-	func testEncodableWrapper_Handles_ArrayOfStringsValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": ["Bar", "Baz"]]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertEqual(testResult, #"{"Foo":["Bar","Baz"]}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_ArrayOfStringsValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": ["Bar", "Baz"]]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertEqual(testResult, #"{"Foo":["Bar","Baz"]}"#)
 	}
 
-	func testEncodableWrapper_Handles_ArrayOfDoublesValue() {
-		do {
-			let anyEncodedObject = try PactBuilder(with: ["Foo": [Double(123.45), Double(789.23)]]).encoded(for: .body).node
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
-			XCTAssertTrue(testResult.contains("789.23")) // NOT THE RIGHT WAY TO TEST THIS! But it will do for now.
-			XCTAssertTrue(testResult.contains(#"{"Foo":[123."#))
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_ArrayOfDoublesValue() throws {
+		let anyEncodedObject = try PactBuilder(with: ["Foo": [Double(123.45), Double(789.23)]]).encoded(for: .body).node
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		XCTAssertTrue(testResult.contains("789.23")) // NOT THE RIGHT WAY TO TEST THIS! But it will do for now.
+		XCTAssertTrue(testResult.contains(#"{"Foo":[123."#))
 	}
 
-	func testEncodableWrapper_Handles_DictionaryValue() {
-		do {
-			let anyEncodedObject =  try PactBuilder(with: ["Foo": ["Bar": "Baz"]]).encoded(for: .body).node
-			let testResult = try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!"))
-			XCTAssertEqual(String(data: testResult, encoding: .utf8), #"{"Foo":{"Bar":"Baz"}}"#)
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+	func testEncodableWrapper_Handles_DictionaryValue() throws {
+		let anyEncodedObject =  try PactBuilder(with: ["Foo": ["Bar": "Baz"]]).encoded(for: .body).node
+		let testResult = try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!"))
+		XCTAssertEqual(String(data: testResult, encoding: .utf8), #"{"Foo":{"Bar":"Baz"}}"#)
 	}
 
-	func testEncodableWrapper_Handles_EmbeddedSafeJSONValues() {
-		do {
-			let anyEncodedObject = try PactBuilder(
-				with: [
-					"Foo": 1,
-					"Bar": 1.23,
-					"Baz": ["Hello", "World"],
-					"Goo": [
-						"one": [1, 23.45],
-						"two": true
-					]
+	func testEncodableWrapper_Handles_EmbeddedSafeJSONValues() throws {
+		let anyEncodedObject = try PactBuilder(
+			with: [
+				"Foo": 1,
+				"Bar": 1.23,
+				"Baz": ["Hello", "World"],
+				"Goo": [
+					"one": [1, 23.45],
+					"two": true
 				]
-			).encoded(for: .body).node
+			]
+		).encoded(for: .body).node
 
-			let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
+		let testResult = try XCTUnwrap(String(data: try JSONEncoder().encode(try XCTUnwrap(anyEncodedObject, "Oh noez!")), encoding: .utf8))
 
-			// WARNING: - This is not the greatest way to test this! But it will do for now.
-			// AnyEncodable `Request.body` is tested in `PactTests.swift` and handles this test on another level
-			XCTAssertTrue(testResult.contains(#""Foo":1"#))
-			XCTAssertTrue(testResult.contains(#""Bar":1.23"#))
-			XCTAssertTrue(testResult.contains(#""Baz":["Hello","World"]"#))
-			XCTAssertTrue(testResult.contains(#""Goo":{"#))
-			XCTAssertTrue(testResult.contains(#""one":[1,23.4"#))
-			XCTAssertTrue(testResult.contains(#""two":true"#))
-		} catch {
-			XCTFail("Failed to unwrap or encode `anEncodedObject` into a `String`")
-		}
+		// WARNING: - This is not the greatest way to test this! But it will do for now.
+		// AnyEncodable `Request.body` is tested in `PactTests.swift` and handles this test on another level
+		XCTAssertTrue(testResult.contains(#""Foo":1"#))
+		XCTAssertTrue(testResult.contains(#""Bar":1.23"#))
+		XCTAssertTrue(testResult.contains(#""Baz":["Hello","World"]"#))
+		XCTAssertTrue(testResult.contains(#""Goo":{"#))
+		XCTAssertTrue(testResult.contains(#""one":[1,23.4"#))
+		XCTAssertTrue(testResult.contains(#""two":true"#))
 	}
 
 	// MARK: - Testing throws
--- Tests/Matchers/RegexLikeTests.swift
+++ Tests/Matchers/RegexLikeTests.swift
@@ -24,13 +24,9 @@ import XCTest
 
 class RegexLikeTests: XCTestCase {
 
-	func testMatcher_RegexLike_InitsWithValue() {
-		do {
-			let testResult = try XCTUnwrap(( Matcher.RegexLike("2020-11-04", term: "\\d{4}-\\d{2}-\\d{2}").value as Any) as? String)
-			XCTAssertEqual(testResult, "2020-11-04")
-		} catch {
-			XCTFail("Failed to unwrap a RegexLike matcher's value")
-		}
+	func testMatcher_RegexLike_InitsWithValue() throws {
+		let testResult = try XCTUnwrap(( Matcher.RegexLike("2020-11-04", term: "\\d{4}-\\d{2}-\\d{2}").value as Any) as? String)
+		XCTAssertEqual(testResult, "2020-11-04")
 	}
 
 }
