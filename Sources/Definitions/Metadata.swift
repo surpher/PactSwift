0ca63eb7203f6378827b8ab49730cd914f96cd6f
Marko Justinek <marko.justinek@gmail.com>
2020-10-23 09:56:01 +1100

    Setup Github workflow for SPM on Xcode 12.1

--- Sources/Definitions/Metadata.swift
+++ Sources/Definitions/Metadata.swift
@@ -20,7 +20,7 @@ import Foundation
 struct Metadata {
 
 	let pactSpec = PactVersion("3.0.0")
-	let pactSwift = PactVersion(Bundle.pact?.shortVersion ?? "0.0.0")
+	let pactSwift = PactVersion(Bundle.pact?.shortVersion ?? "0.2.0")
 
 	struct PactVersion: Encodable {
 		let version: String
