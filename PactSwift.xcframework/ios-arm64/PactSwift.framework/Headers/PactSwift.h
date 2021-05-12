7a1231b51d34f22e074ec6be6b7d31bd258bd944
Marko Justinek <marko.justinek@ittybittyapps.com>
2021-05-12 12:51:41 +1000

    feat: Expose PactSwift as XCFramework

---
+++ PactSwift.xcframework/ios-arm64/PactSwift.framework/Headers/PactSwift.h
@@ -0,0 +1,26 @@
+//
+//  Created by Marko Justinek on 26/3/20.
+//  Copyright © 2020 Marko Justinek. All rights reserved.
+//
+//  Permission to use, copy, modify, and/or distribute this software for any
+//  purpose with or without fee is hereby granted, provided that the above
+//  copyright notice and this permission notice appear in all copies.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+//  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+//  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+//  SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+//  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+//  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+//  IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PactSwift.
+FOUNDATION_EXPORT double PactSwiftVersionNumber;
+
+//! Project version string for PactSwift.
+FOUNDATION_EXPORT const unsigned char PactSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PactSwift/PublicHeader.h>
