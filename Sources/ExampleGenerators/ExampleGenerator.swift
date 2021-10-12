207812b02f04ab646cd019a69e5e22ae65dbb063
Marko Justinek <marko.justinek@gmail.com>
2021-10-12 17:56:59 +1100

    doco: Add high level explanations
    Matchers and Example Generators in PactSwift are defined as types. They are Encodable types that conform to Pact specification v3 structure. Adding some high level explanation of how they convert from PactSwift model into JSON structure that conforms to Pact specification.

--- Sources/ExampleGenerators/ExampleGenerator.swift
+++ Sources/ExampleGenerators/ExampleGenerator.swift
@@ -20,11 +20,69 @@ import Foundation
 public struct ExampleGenerator {
 	// This is a namespace placeholder
 	// Implement any Example Generators as `Struct`s in an extension.
+
+	// ⚠️ IMPORTANT ⚠️
+	// Make sure PactBuilder.swift handles the example generator
+	// There is a bug in Swift where protocols are not handled properly when
+	// used as generics.
+
+	// Example Generators are Encodable objects that conform to the defined structure.
+	// When DSL is being processed the Example Generator object is created and added to Pact.
+	// Every Example Generator conforms to ExampleGeneratorExpressible protocol which
+	// defines that at least `type` key is provided. Generally an Example Generator would
+	// set its type from the `generator` property. That then matches the agreed generator name
+	// and attributes defined in Pact Specification version 3 (see link below). The example values
+	// generated in PactSwift are re-generated with each test run and are re-generated at
+	// mock server point (when running `PactSwiftMockServer.verify()`).
+	//
+	// We use `AnyEncodable` type eraser because we can not predict what type the user will provide.
+	//
+	// As an example, the `RandomInt` example generator would use `ExampleGenerator.Generator`
+	// and dictionary `rules` defines the attributes to of the generated example value.
+	//
+	// Imagine the following properties of an Example Generator:
+	//
+	//    let value: Any
+	//    let generator: Example.Generator = .int
+	//    let rules: [String: AnyEncodable] = ["min": AnyEncodable(3), "max": AnyEncodable(9)]
+	//
+	// would generate an object for the jsonPath where the example generator was used:
+	//
+	//    {
+	//      "type": "RandomInt",
+	//      "min": 3,
+	//      "max": 9
+	//    }
+	//
+	// This JSON object is applied to the specific jsonPath whilst the DSL structure is being processed.
+	//
+	// Example:
+	//
+	//    // DSL
+	//    let body = [
+	//      "randomInt": ExampleGenerator.RandomInt(min: 3, max: 9)
+	//    ]
+	//
+	//    // Extract from Pact contract (JSON file)
+	//    "generators": {
+	//      "body": {
+	//        "$.randomInt": {
+	//          "type": "RandomInt",
+	//          "min": 3,
+	//          "max": 9
+	//        }
+	//      }
+	//    }
+	//
+	// See: https://github.com/pact-foundation/pact-specification/tree/version-3#introduce-example-generators
+	//
+
 }
 
 extension ExampleGenerator {
 
-	// A list of implemented Example Generators that map to a generator in Pact Specification.
+	// A list of implemented Example Generators that map to a generator in Pact Specification
+	// See: https://github.com/pact-foundation/pact-specification/tree/version-3#introduce-example-generators
 	enum Generator: String {
 		case bool = "RandomBoolean"
 		case date = "Date"
@@ -43,6 +101,7 @@ extension ExampleGenerator {
 
 // MARK: - Objective-C
 
+/// Acts as a bridge defining the Swift specific Generator type
 protocol ObjcGenerator {
 
 	var type: ExampleGeneratorExpressible { get }
